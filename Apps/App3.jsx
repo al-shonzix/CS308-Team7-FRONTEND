aced29897be3e12b5abceaae4e462106412a20bb
Taha86-cyber <76820816+Taha86-cyber@users.noreply.github.com>
2024-05-27 18:49:55 +0300

    Add files via upload

---
+++ Apps/App3.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { MyForm } from "../Components/MyForm";
+import Layout from "../Components/layout/Layout";
+import "./App3.css";
+
+const App3 = () => {
+  return (
+    <div className="App3">
+      <Layout>
+        <MyForm />
+      </Layout>
+    </div>
+  );
+}
+
+export default App3;
